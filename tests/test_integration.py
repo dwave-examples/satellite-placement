2cea37bfa6a16fb4b7d64fd87391a94ab394bfb7
Hemant Pawar <hpawar@dwavesys.com>
2020-05-14 20:22:51 +0000

    Updated as per review comments

--- tests/test_integration.py
+++ tests/test_integration.py
@@ -1,16 +1,16 @@
-#    Copyright 2020 D-Wave Systems Inc.
-
-#    Licensed under the Apache License, Version 2.0 (the "License")
-#    you may not use this file except in compliance with the License.
-#    You may obtain a copy of the License at
-
-#        http: // www.apache.org/licenses/LICENSE-2.0
-
-#    Unless required by applicable law or agreed to in writing, software
-#    distributed under the License is distributed on an "AS IS" BASIS,
-#    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-#    See the License for the specific language governing permissions and
-#    limitations under the License.
+# Copyright 2020 D-Wave Systems Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 
 import subprocess
 import unittest
@@ -19,10 +19,10 @@ import os
 class IntegrationTests(unittest.TestCase):
 
     def test_satellite(self):
-        cwd = os.getcwd()
-        output = subprocess.check_output(["python", cwd+"/satellite.py"])
+        output = subprocess.check_output(["python", "satellite.py"])
         output = str(output).upper()
-        print("Example output \n"+ output)
+        if os.getenv('DEBUG_OUTPUT'):
+            print("Example output \n"+ output)
 
         with self.subTest(msg="Verify if output contains '[frozenset({' \n"):
             self.assertIn("[frozenset({".upper(), output)
