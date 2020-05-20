dc94f62040557ba753e508ab4baf8baa05240f89
Hemant Pawar <hpawar@dwavesys.com>
2020-05-20 22:58:14 +0000

    Updated formatting

--- tests/test_satellite.py
+++ tests/test_satellite.py
@@ -37,7 +37,7 @@ class TestSmoke(unittest.TestCase):
         output = subprocess.check_output([sys.executable, file_path])
         output = str(output).upper()
         if os.getenv('DEBUG_OUTPUT'):
-            print("Example output \n"+ output)
+            print("Example output \n" + output)
 
         with self.subTest(msg="Verify if output contains '[frozenset({' \n"):
             self.assertIn("[frozenset({".upper(), output)
