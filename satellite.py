104d42bf64d5f926895e0baaf48ae2ba07a38698
Joel Gottlieb <jgottlieb@dwavesys.com>
2020-08-13 11:29:35 -0700

    Moving the bigger problem into one program

--- satellite.py
+++ satellite.py
@@ -32,15 +32,16 @@ import random
 
 import dimod
 import neal
+from dwave.system import LeapHybridSampler
 import networkx as nx
+import sys
 
+# Original problem
 # we wish to divide 12 satellites into 4 constellations of 3 satellites each.
 num_satellites = 12
 num_constellations = 4
 
-constellation_size = num_satellites // num_constellations
-
-
+# Original problem
 # each of the 12 satellites (labelled 0-11) has a coverage score. This could be
 # calculated as the percentage of time that the Earth region is in range of the
 # satellite
@@ -56,6 +57,55 @@ coverage = {0: 0.90,
             9: 0.99,
             10: 0.25,
             11: 0.91}
+sampler = neal.Neal()
+
+if len(sys.argv) > 1:
+
+    num_satellites = 39
+    num_constellations = 13
+    coverage = {0: 0.90,
+                1: 0.82,
+                2: 0.92,
+                3: 0.14,
+                4: 0.43,
+                5: 0.62,
+                6: 0.78,
+                7: 0.48,
+                8: 0.94,
+                9: 0.21,
+                10: 0.97,
+                11: 0.61,
+                12: 0.11,
+                13: 0.93,
+                14: 0.65,
+                15: 0.10,
+                16: 0.87,
+                17: 0.64,
+                18: 0.29,
+                19: 0.96,
+                20: 0.23,
+                21: 0.45,
+                22: 0.56,
+                23: 0.31,
+                24: 0.09,
+                25: 0.85,
+                26: 0.34,
+                27: 0.80,
+                28: 0.75,
+                29: 0.41,
+                30: 0.66,
+                31: 0.55,
+                32: 0.01,
+                33: 0.30,
+                34: 0.15,
+                35: 0.60,
+                36: 0.76,
+                37: 0.70,
+                38: 0.84}
+    sampler = LeapHybridSampler()
+
+constellation_size = num_satellites // num_constellations
+    
 
 # don't consider constellations with average score less than score_threshold
 score_threshold = .4
@@ -90,6 +140,46 @@ bqm.update(dimod.generators.combinations(bqm.variables, num_constellations, stre
 
 # sample from the bqm using simulated annealing
 sampleset = neal.Neal().sample(bqm, num_reads=100).aggregate()
+
+constellations = [constellation
+                  for constellation, chosen in sampleset.first.sample.items()
+                  if chosen]
+
+print(constellations)
+
+# don't consider constellations with average score less than score_threshold
+score_threshold = .4
+
+bqm = dimod.BinaryQuadraticModel.empty(dimod.BINARY)
+
+# first we want to favor combinations with a high score
+for constellation in itertools.combinations(range(num_satellites), constellation_size):
+    # the score is the average coverage for a constellation
+    score = sum(coverage[v] for v in constellation) / constellation_size
+
+    # to make it smaller, throw out the combinations with a score below
+    # a set threshold
+    if score < score_threshold:
+        continue
+
+    # we subtract the score because we want to minimize the energy
+    bqm.add_variable(frozenset(constellation), -score)
+
+# next we want to penalize pairs that share a satellite. We choose 2 because
+# because we don't want it to be advantageous to pick both in the case that
+# they both have 100% coverage
+for c0, c1 in itertools.combinations(bqm.variables, 2):
+    if c0.isdisjoint(c1):
+        continue
+    bqm.add_interaction(c0, c1, 2)
+
+# finally we wish to choose num_constellations variables. We pick strength of 1
+# because we don't want it to be advantageous to violate the constraint by
+# picking more variables
+bqm.update(dimod.generators.combinations(bqm.variables, num_constellations, strength=1))
+
+# sample from the bqm using simulated annealing
+sampleset = LeapHybridSampler().sample(bqm).aggregate()
 
 constellations = [constellation
                   for constellation, chosen in sampleset.first.sample.items()
